5eaa97f7e277dbd25b4f76cfbff90f3606fe261e
Mohd Shariq Ansari <ansari.shariq.badshah@gmail.com>
2025-11-03 04:21:57 +0530

    second try

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    console.log('MONGODB_URI:', process.env.MONGODB_URI);
+
+    if (!process.env.MONGODB_URI) {
+      throw new Error('MONGODB_URI is not defined in environment variables');
+    }
+
     const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
